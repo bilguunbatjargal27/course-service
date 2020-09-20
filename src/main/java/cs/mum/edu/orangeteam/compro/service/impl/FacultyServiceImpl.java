c539dc475aa0e4678c3fb754666bb1762f7808a0
kirubelandargie <kandargie@miu.edu>
2020-09-20 17:10:53 -0500

    faculty-crud

---
+++ src/main/java/cs/mum/edu/orangeteam/compro/service/impl/FacultyServiceImpl.java
@@ -0,0 +1,41 @@
+package cs.mum.edu.orangeteam.compro.service.impl;
+
+import cs.mum.edu.orangeteam.compro.DAO.FacultyRepository;
+import cs.mum.edu.orangeteam.compro.model.Faculty;
+import cs.mum.edu.orangeteam.compro.service.FacultyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+@Service
+public class FacultyServiceImpl implements FacultyService {
+
+    @Autowired
+    private FacultyRepository facultyRepository;
+
+    @Override
+    public Faculty addFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    @Override
+    public Faculty updateFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    @Override
+    public void deleteFacultyById(Long id) {
+        facultyRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Faculty findFacultyById(Long id) {
+        return facultyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Collection<Faculty> findAllFaculty() {
+        return facultyRepository.findAll();
+    }
+}
