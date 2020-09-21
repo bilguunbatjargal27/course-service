bb34ec802d0dcae7d6bfd02af0a4e30b1358c084
Bilguun Batjargal <bilguunbatjargal27@gmail.com>
2020-09-20 21:55:09 -0500

    g

--- src/main/java/cs/mum/edu/orangeteam/compro/controller/StudentController.java
+++ src/main/java/cs/mum/edu/orangeteam/compro/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("course/students")
@@ -23,6 +24,12 @@ public class StudentController {
     @GetMapping("/{id}")
     public Student getStudentById(@PathVariable("id") Long id){
         return studentService.findStudentById(id);
+    }
+
+
+    @GetMapping("/getNames")
+    public List<String> getStudentNames(){
+        return studentService.findAll().stream().map(Student::getName).collect(Collectors.toList());
     }
 
 
--- src/main/java/cs/mum/edu/orangeteam/compro/model/Course.java
+++ src/main/java/cs/mum/edu/orangeteam/compro/model/Course.java
@@ -39,4 +39,84 @@ public class Course {
 
     @OneToMany
     private List<Attend> attends = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isTA() {
+        return isTA;
+    }
+
+    public void setTA(boolean TA) {
+        isTA = TA;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    public List<Attend> getAttends() {
+        return attends;
+    }
+
+    public void setAttends(List<Attend> attends) {
+        this.attends = attends;
+    }
 }
