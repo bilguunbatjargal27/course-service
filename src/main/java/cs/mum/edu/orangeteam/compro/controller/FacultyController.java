f96b99f638693e831d2873376a38f66eaa94c3fb
kirubelandargie <kandargie@miu.edu>
2020-09-22 22:09:34 -0500

    change controller to response entity

--- src/main/java/cs/mum/edu/orangeteam/compro/controller/FacultyController.java
+++ src/main/java/cs/mum/edu/orangeteam/compro/controller/FacultyController.java
@@ -1,11 +1,14 @@
 package cs.mum.edu.orangeteam.compro.controller;
 
 import cs.mum.edu.orangeteam.compro.model.Faculty;
-import cs.mum.edu.orangeteam.compro.model.Student;
 import cs.mum.edu.orangeteam.compro.service.FacultyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,28 +18,38 @@ public class FacultyController {
     private FacultyService facultyService;
 
     @GetMapping("")
-    public List<Faculty>getAllFaculties(){
-        return (List<Faculty>) facultyService.findAllFaculty();
+    public ResponseEntity<?> getAllFaculties(){
+        List<Faculty> faculties=(List<Faculty>) facultyService.findAllFaculty();
+        return ResponseEntity.ok(faculties);
     }
     @GetMapping("/{id}")
-    public Faculty getFacultyById(@PathVariable Long id){
-        return facultyService.findFacultyById(id);
+    public ResponseEntity<?> getFacultyById(@PathVariable Long id){
+       Faculty faculty= facultyService.findFacultyById(id);
+        return ResponseEntity.ok(faculty);
     }
     @PostMapping("/add")
-    public Faculty addFaculty(@RequestBody final Faculty faculty){
-        return facultyService.addFaculty(faculty);
+    public ResponseEntity<?> addFaculty(@Valid @RequestBody final Faculty faculty, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+        facultyService.addFaculty(faculty);
+        return ResponseEntity.status(HttpStatus.OK).body("Faculty is created successfully");
     }
 
     @PutMapping("/update")
-    public Faculty updateFaculty(@RequestBody final Faculty faculty){
-        return facultyService.updateFaculty(faculty);
+    public ResponseEntity<?> updateFaculty(@Valid @RequestBody final Faculty faculty,BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+        Faculty fac=facultyService.updateFaculty(faculty);
+        return ResponseEntity.status(HttpStatus.OK).body("Faculty is updated successfully");
     }
 
     @DeleteMapping("/delete/{id}")
-    public boolean deleteFaculty(@PathVariable("id") Long id){
+    public ResponseEntity<?> deleteFaculty(@PathVariable("id") Long id){
         Faculty faculty = facultyService.findFacultyById(id);
-        if(faculty == null) return false;
+        if(faculty == null) return ResponseEntity.badRequest().body("There is no faculty has an id equal to" + id);
        facultyService.deleteFacultyById(id);
-        return true;
+        return ResponseEntity.status(HttpStatus.OK).body("Faculty is deleted successfully");
     }
 }
