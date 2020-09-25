97b91e497c9f3bbef15ce935081a8caaf28d4977
Bilguun Batjargal <bilguunbatjargal27@gmail.com>
2020-09-25 13:14:46 -0500

    last

--- src/main/java/cs/mum/edu/orangeteam/compro/DAO/CPTReport.java
+++
@@ -1,9 +0,0 @@
-package cs.mum.edu.orangeteam.compro.DAO;
-
-import java.util.Date;
-
-public class CPTReport {
-    private Long id;
-    private String cptReport;
-    private Date submittedDate;
-}
